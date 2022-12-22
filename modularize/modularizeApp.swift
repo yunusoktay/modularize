c90430633cabf60aae5046eb29f38aa17c8fdf90
yunusoktay <ynsokty.3@gmail.com>
2022-12-22 15:32:28 +0300

    Initial Commit

---
+++ modularize/modularizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  modularizeApp.swift
+//  modularize
+//
+//  Created by yunus oktay on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct modularizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
